72ecd5237a1ad387421b7c94797fa5cf98681420
Muaz Ahmad <muaz@Muazs-MacBook-Air.local>
2023-12-09 22:07:54 -0500

    plzgod

--- Muaz-Website/vite.config.js
+++ Muaz-Website/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import font from './assets/Sketch.ttf'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  base: '/Muaz-Website/',
   plugins: [react()],
   build: {
     outDir: 'dist', // Specify the output directory
